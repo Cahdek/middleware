ac55b1b240b51e421038089c3c3e9a8e8e4a22ae
Doug Hunting <doug@huntingperformance.com>
2022-11-13 09:06:13 -0800

    middleware
    - Initial Commit with doctrine installed and configured
    - Added default AbstractModel

---
+++ config/autoload/doctrine.global.php
@@ -0,0 +1,173 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Doctrine\CustomCacheProvider;
+use Doctrine\Common\Cache\ApcuCache;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\Common\Cache\ChainCache;
+use Doctrine\Common\Cache\FilesystemCache;
+use Doctrine\Common\Cache\MemcacheCache;
+use Doctrine\Common\Cache\MemcachedCache;
+use Doctrine\Common\Cache\PhpFileCache;
+use Doctrine\Common\Cache\PredisCache;
+use Doctrine\Common\Cache\RedisCache;
+use Doctrine\Common\Cache\WinCacheCache;
+use Doctrine\Common\Cache\XcacheCache;
+use Doctrine\Common\Cache\ZendDataCache;
+use Doctrine\DBAL\Driver\PDO\MySQL\Driver;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\Persistence\Mapping\Driver\MappingDriverChain;
+use Roave\PsrContainerDoctrine\EntityManagerFactory;
+
+return [
+    'doctrine'     => [
+        'configuration'  => [
+            'orm_default' => [
+                'result_cache'                  => 'filesystem',
+                'metadata_cache'                => 'filesystem',
+                'query_cache'                   => 'filesystem',
+                'hydration_cache'               => 'filesystem',
+                'driver'                        => 'orm_default', // Actually defaults to the configuration config key, not hard-coded
+                'auto_generate_proxy_classes'   => true,
+                'proxy_dir'                     => '/../../data/cache/DoctrineEntityProxy',
+                'proxy_namespace'               => 'DoctrineEntityProxy',
+                'entity_namespaces'             => [],
+                'datetime_functions'            => [],
+                'string_functions'              => [],
+                'numeric_functions'             => [],
+                'filters'                       => [],
+                'named_queries'                 => [],
+                'named_native_queries'          => [],
+                'custom_hydration_modes'        => [],
+                'naming_strategy'               => null,
+                'quote_strategy'                => null,
+                'default_repository_class_name' => null,
+                'repository_factory'            => null,
+                'class_metadata_factory_name'   => null,
+                'entity_listener_resolver'      => null,
+                'second_level_cache'            => [
+                    'enabled'                    => false,
+                    'default_lifetime'           => 3600,
+                    'default_lock_lifetime'      => 60,
+                    'file_lock_region_directory' => '',
+                    'regions'                    => [],
+                ],
+                'sql_logger'                    => null,
+                'middlewares'                   => [
+                    // List of middlewares doctrine will use to decorate the `Driver` component.
+                    // (see https://github.com/doctrine/dbal/blob/3.3.2/docs/en/reference/architecture.rst#middlewares)
+                ],
+            ],
+        ],
+        'connection'     => [
+            'orm_default' => [
+                'driver_class'             => Driver::class,
+                'wrapper_class'            => null,
+                'pdo'                      => null,
+                'configuration'            => 'orm_default', // Actually defaults to the connection config key, not hard-coded
+                'event_manager'            => 'orm_default', // Actually defaults to the connection config key, not hard-coded
+                'params'                   => [],
+                'doctrine_mapping_types'   => [],
+                'doctrine_commented_types' => [],
+            ],
+        ],
+        'entity_manager' => [
+            'orm_default' => [
+                'connection'    => 'orm_default', // Actually defaults to the entity manager config key, not hard-coded
+                'configuration' => 'orm_default', // Actually defaults to the entity manager config key, not hard-coded
+            ],
+        ],
+        'event_manager'  => [
+            'orm_default' => [
+                'subscribers' => [],
+                'listeners'   => [],
+            ],
+        ],
+        'driver'         => [
+            'orm_default' => [
+                'class'           => MappingDriverChain::class,
+                'paths'           => [],
+                'extension'       => null,
+                'drivers' => [
+                    'App\Entity' => 'app_entity',
+                ],
+                'global_basename' => null,
+                'default_driver'  => null,
+            ],
+            'app_entity' => [
+                'class' => AnnotationDriver::class,
+                'cache' => 'array',
+                'paths' => [
+                    __DIR__ . '/../../src/',
+                ],
+            ],
+        ],
+        'cache'          => [
+            'apcu'              => [
+                'class'     => ApcuCache::class,
+                'namespace' => 'psr-container-doctrine',
+            ],
+            'array'             => [
+                'class'     => ArrayCache::class,
+                'namespace' => 'psr-container-doctrine',
+            ],
+            'filesystem'        => [
+                'class'     => FilesystemCache::class,
+                'directory' => 'data/cache/doctrine',
+                'namespace' => 'app',
+            ],
+            'memcache'          => [
+                'class'     => MemcacheCache::class,
+                'instance'  => 'my_memcache_alias',
+                'namespace' => 'psr-container-doctrine',
+            ],
+            'memcached'         => [
+                'class'     => MemcachedCache::class,
+                'instance'  => 'my_memcached_alias',
+                'namespace' => 'psr-container-doctrine',
+            ],
+            'phpfile'           => [
+                'class'     => PhpFileCache::class,
+                'directory' => 'data/cache/DoctrineCache',
+                'namespace' => 'psr-container-doctrine',
+            ],
+            'predis'            => [
+                'class'     => PredisCache::class,
+                'instance'  => 'my_predis_alias',
+                'namespace' => 'psr-container-doctrine',
+            ],
+            'redis'             => [
+                'class'     => RedisCache::class,
+                'instance'  => 'my_redis_alias',
+                'namespace' => 'psr-container-doctrine',
+            ],
+            'wincache'          => [
+                'class'     => WinCacheCache::class,
+                'namespace' => 'psr-container-doctrine',
+            ],
+            'xcache'            => [
+                'class'     => XcacheCache::class,
+                'namespace' => 'psr-container-doctrine',
+            ],
+            'zenddata'          => [
+                'class'     => ZendDataCache::class,
+                'namespace' => 'psr-container-doctrine',
+            ],
+            'my_cache_provider' => [
+                'class' => CustomCacheProvider::class, //The class is looked up in the container
+            ],
+            'chain'             => [
+                'class'     => ChainCache::class,
+                'providers' => ['array', 'redis'], // you can use any provider listed above
+                'namespace' => 'psr-container-doctrine', // will be applied to all providers in the chain
+            ],
+        ],
+        'types'          => [],
+    ],
+    'dependencies' => [
+        'factories' => [
+            'doctrine.entity_manager.orm_default' => EntityManagerFactory::class,
+        ],
+    ],
+];
---
+++ src/App/src/Entity/AbstractModel.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\MappedSuperclass
+ * @ORM\HasLifecycleCallbacks
+ */
+abstract class AbstractModel
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     *
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="datetime",columnDefinition="DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP")
+     *
+     * @var DateTime
+     */
+    protected $created;
+
+    /**
+     * @ORM\Column(type="datetime",columnDefinition="DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+     *
+     * @var DateTime
+     */
+    protected $modified;
+
+    /**
+     * @return number
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getCreated(): DateTime
+    {
+        return $this->created;
+    }
+
+    public function getModified(): DateTime
+    {
+        return $this->modified;
+    }
+
+    /**
+     * @param number $id
+     */
+    public function setId($id): AbstractModel
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @param DateTime $created
+     */
+    public function setCreated($created): AbstractModel
+    {
+        $this->created = $created;
+        return $this;
+    }
+
+    /**
+     * @param DateTime $modified
+     */
+    public function setModified($modified): AbstractModel
+    {
+        $this->modified = $modified;
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setDateCreated(): AbstractModel
+    {
+        $this->created = new DateTime();
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function updateDateModified(): AbstractModel
+    {
+        $this->modified = new DateTime();
+        return $this;
+    }
+}
