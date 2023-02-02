907bdf1edb990a60e48d6ba39605b9d5483a6901
Doug Hunting <doug@huntingperformance.com>
2023-02-01 18:56:28 -0800

    middleware
    - Removed templates from ConfigProvider

--- src/App/src/ConfigProvider.php
+++ src/App/src/ConfigProvider.php
@@ -17,36 +17,21 @@ class ConfigProvider
      * To add a bit of a structure, each section is defined in a separate
      * method which returns an array with its configuration.
      */
-    public function __invoke() : array
+    public function __invoke(): array
     {
         return [
             'dependencies' => $this->getDependencies(),
-            'templates'    => $this->getTemplates(),
         ];
     }
 
     /**
      * Returns the container dependencies
      */
-    public function getDependencies() : array
+    public function getDependencies(): array
     {
         return [
-            'invokables' => [
-            ],
-            'factories'  => [
-            ],
-        ];
-    }
-
-    /**
-     * Returns the templates configuration
-     */
-    public function getTemplates() : array
-    {
-        return [
-            'paths' => [
-                'app'    => [__DIR__ . '/../templates/'],
-            ],
+            'invokables' => [],
+            'factories'  => [],
         ];
     }
 }
